7dc5a0e480c10a706aa76e1107ab327d076bde9c
Google <kuzmietsovaiaryna@gmail.com>
2025-04-06 17:45:52 +0300

    3232

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const fs = require('fs')
 const path = require('path');
-const pathToIndex = path.join(__dirname, 'static', 'index.html')
-const indexHtmlFile = fs.readFileSync(pathToIndex)
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'))
+const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'))
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'))
 const server = http.createServer((req,res)=>{
+    switch(req.url) {
+        case '/': return res.end(indexHtmlFile)
+        case '/script.js': return res.end(scriptFile)
+        case '/style.css': return res.end(styleFile)
+    }
     if(req.url === '/'){
         return res.end(indexHtmlFile)
     }
