70890803a0353af3528bfd4621649e5e3277718b
Google <kuzmietsovaiaryna@gmail.com>
2025-05-04 17:45:17 +0300

    8889

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 const path = require('path');
-const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'))
-const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'))
-const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'))
-const server = http.createServer((req,res)=>{
+const db = require('./database');
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
+const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+const registerFile = fs.readFileSync(path.join(__dirname, 'static', 'register.html'));
+
+const server = http.createServer((req, res) => {
     switch(req.url) {
-        case '/': return res.end(indexHtmlFile)
-        case '/script.js': return res.end(scriptFile)
-        case '/style.css': return res.end(styleFile)
+        case '/': return res.end(indexHtmlFile);
+        case '/script.js': return res.end(scriptFile);
+        case '/style.css': return res.end(styleFile);
+        case '/register': return res.end(registerFile);
     }
-    if(req.url == '404'){
-        return res.end(indexHtmlFile)
-    }
-    res.statusCode = 404;
-    return res.end('Error 404')
+    return res.end('Error 404');
 });
 server.listen(3000);
+const { Server } = require("socket.io");
+const io = new Server(server);
+io.on('connection', async (socket) => {
+  console.log('a user connected. id - ' + socket.id);
+  let userNickname = 'admin';
+  let messages = await db.getMessages();
+  socket.emit('all_messages', messages);
+  socket.on('new_message', (message) => {
+    db.addMessage(message, 1);
+    io.emit('message', userNickname + ' : ' + message);
+  });
+});
